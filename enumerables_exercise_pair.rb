dc6b2a16d4e0ec1d1aab398e201d54a31d5ec0c0
mrossant <matteorossant@gmail.com>
2021-08-23 15:38:52 -0400

    my_flatten

--- enumerables_exercise_pair.rb
+++ enumerables_exercise_pair.rb
@@ -52,14 +52,33 @@ class Array
         end
         true
     end
+
+    def my_flatten
+        answer = []
+        self.each do |el|
+            if !el.is_a?(Array)
+                answer << el
+            else
+                el.each { |inst| answer << inst }
+            end
+        end
+
+        if answer.all? {|el| !el.is_a?(Array)}
+            return answer
+        else
+            answer.my_flatten
+        end
+    end
 end  
 
 
-a = [1, 2, 3]
-# p a.my_any? { |num| num > 1 } # => true
-# p a.my_any? { |num| num == 4 } # => false
-p a.my_all? { |num| num > 1 } # => false
-p a.my_all? { |num| num < 4 } # => true
+# a = [1, 2, 3]
+# # p a.my_any? { |num| num > 1 } # => true
+# # p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
     
 
