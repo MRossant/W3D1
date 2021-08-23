61cd86d2c5f24b4d61c36beccd416cf650acddf0
tharpercp <tharpercp@gmail.com>
2021-08-23 12:21:55 -0700

    my_all & my_any

--- enumerables_exercise_pair.rb
+++ enumerables_exercise_pair.rb
@@ -2,6 +2,8 @@ require 'byebug'
 
 class Array
 
+
+
     def my_each(&prc)
         i = 0
         while i < self.length
@@ -22,11 +24,42 @@ class Array
 
         result
     end
+
+    def my_reject(&prc)
+        result = []
+        i = 0
+        while i < self.length
+            result << self[i] if !prc.call(self[i])
+            i += 1
+        end
+        result
+    end
     
-    
-end
+    def my_any?(&prc)
+        i = 0
+        while i < self.length
+            return true if prc.call(self[i])
+            i += 1
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        i = 0
+        while i < self.length
+            return false if !prc.call(self[i])
+            i += 1
+        end
+        true
+    end
+end  
+
 
 a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
+
+    
 
