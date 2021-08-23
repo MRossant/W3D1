21e3689ef3fc1fbea9ee9536b3a543cd184f4976
mrossant <matteorossant@gmail.com>
2021-08-23 14:57:08 -0400

    my-each

--- enumerables_exercise_pair.rb
+++ enumerables_exercise_pair.rb
@@ -1,19 +1,31 @@
 require 'byebug'
 
 class Array
+
     def my_each(&prc)
         i = 0
         while i < self.length
-            self[i] = prc.call(self[i])
+            prc.call(self[i]) # { |num| num > 1 }
             i += 1
         end
-
+        debugger
         self
     end
 
-    def my_select()
+    def my_select(&prc)
+        result = []
+        i = 0
+        while i < self.length
+            result << self[i] if self.my_each(&prc) # returns array
+            i += 1
+        end
+
+        result
+    end
 end
 
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 }
 
 
 
